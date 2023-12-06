3661ca9630bc8b8c4c27424897079405e4d819dd
julien <nttmxk@korea.ac.kr>
2023-12-06 15:43:03 +0900

    new: initial status

---
+++ Server.hpp
@@ -0,0 +1,11 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <iostream>
+# include <cstring>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <unistd.h>
+# define BUF_LEN 1024
+
+#endif
