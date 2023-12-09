cf09d2bdc490a5dd33d2120d6f578cce1fce39ce
julien <nttmxk@korea.ac.kr>
2023-12-09 17:58:11 +0900

    feat: add addClient, readMessage function
    remove clientFds, clientNum variable

--- Server.hpp
+++ Server.hpp
@@ -7,6 +7,7 @@
 # include <netinet/in.h>
 # include <unistd.h>
 # include <poll.h>
+
 # define BUF_LEN 1024
 # define USER_MAX 128
 
@@ -16,28 +17,23 @@ public:
 	Server();
 	~Server();
 	Server(const Server &orig);
-	Server& operator=(const Server &orig);
+	Server &operator=(const Server &orig);
 
 	int				getServerFd(void) const;
-	int			 	getClientNum(void) const;
-	struct pollfd*	getPollFds(void) const;
-	int				addClient(void);
-	int 			readMessage(int clientFd);
+	struct pollfd	*getPollFds(void) const;
+	void			addClient(void);
+	void			readMessage(int clientFd);
 	void			runCommand(int clientFd);
 
 private:
-	int						serverFd;
-	struct sockaddr_in		serverAddress;
-//	int 					clientFds[USER_MAX];
-	char 					buffer[BUF_LEN];
-	struct pollfd			pollFds[USER_MAX + 1];
-	int 					_clientNum;
-	std::string 			_pwd;
-	void					setPoll(int fd, short events, short revents);
-	// vector / list / map?
-	//std::list<Channel>
-	//std::list<Client>
+	int					serverFd;
+	struct sockaddr_in	serverAddress;
+	char				buffer[BUF_LEN];
+	struct pollfd		pollFds[USER_MAX + 4];
+	std::string			_pwd;
 
+	void				setPoll(int index, int fd, short events, short revents);
+	//std::list<Channel>
 };
 
 #endif
